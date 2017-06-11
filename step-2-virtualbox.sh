c1ecf0fbe12a7dc21ac96b18ea4eaa26e903d41c
Geoff Rich <geoff.rich@gmail.com>
2017-06-11 10:48:28 +1000

    Add NAT rule for virtualbox.

--- step-2-virtualbox.sh
+++ step-2-virtualbox.sh
@@ -3,6 +3,7 @@
 # Create all in one image using packer
 packer build --only=virtualbox-ovf allinoneserver.json
 
-# Start up the new servers!
+# Start up the new servers! Add in port forwarding assuming its NAT'd
 VBoxManage import ./virtualbox/allinone-server/allinone-server.ovf
 VBoxManage startvm allinone-server
+VBoxManage controlvm "allinone-server" natpf1 "guestssh,tcp,127.0.0.1,9022,,22"
