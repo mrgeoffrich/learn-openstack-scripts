664ecbaedac504161f3638fdc2cbbb099f202d9f
Geoff Rich <geoff.rich@gmail.com>
2017-06-24 12:57:33 +1000

    Match documented steps.

--- step-2-virtualbox.sh
+++ step-2-virtualbox.sh
@@ -5,5 +5,12 @@ packer build --only=virtualbox-ovf allinoneserver.json
 
 # Start up the new servers! Add in port forwarding assuming its NAT'd
 VBoxManage import ./virtualbox/allinone-server/allinone-server.ovf
-VBoxManage startvm allinone-server
-VBoxManage controlvm "allinone-server" natpf1 "guestssh,tcp,127.0.0.1,9022,,22"
+
+# List all the available bridge interfaces
+VBoxManage list bridgedifs
+
+echo 'From that list of interfaces, pick which network to attach the VM to. Say it is en0, run the following:'
+echo 'VBoxManage modifyvm "allinone-server" --nic1 bridged'
+echo 'VBoxManage modifyvm "allinone-server" --bridgeadapter1 en0'
+echo 'VBoxManage modifyvm "allinone-server" --nicpromisc1 allow-all'
+echo 'VBoxManage startvm allinone-server'
--- step-2-vmware.sh
+++ step-2-vmware.sh
@@ -5,3 +5,5 @@ packer build --only=vmware-vmx allinoneserver.json
 
 # Start up the new servers!
 /Applications/VMware\ Fusion.app/Contents/Library/vmrun -T fusion start ./vmware/allinone-server/allinone-server.vmx
+
+echo 'Ensure that the VM is connected as bridged networking to the same network as your Mac.'
