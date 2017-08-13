7386f328e98caccbb6eea3b049f029483aaf20ca
Geoff Rich <geoff.rich@gmail.com>
2017-08-13 10:43:06 +1000

    Add hyper-v build for deploy server

--- step-3-hyperv.ps1
+++ step-3-hyperv.ps1
@@ -1,3 +1,31 @@
-Write-Host "Please ensure you have configured an External switch, and in deployserverhyperv.json you have"
+function Import-Packer-VM
+{
+    Param(
+        [parameter(Mandatory=$true)]
+        [string]
+        $VMPath
+        )
+    
+    $files = Get-ChildItem -Path $VMPath -Filter '*.vmcx'
+    $vmcxFilePath = $VMPath + '\' + $files[0].Name
+    Import-VM $vmcxFilePath  -Copy -GenerateNewId
+}
+
+Write-Host "Please ensure you have configured an External switch, and in the packer config you have"
 Write-Host "replaced the hyperv_switch variable value with the name of this switch."
-packer build --only=hyperv-iso .\deployserverhyperv.json
+
+$multinodeFile = "multinode-vars.json"
+
+packer build --only=hyperv-iso -var-file="$multinodeFile" .\multinode-deploy-hyperv.json
+packer build --only=hyperv-iso -var-file="$multinodeFile" .\multinode-compute-hyperv.json
+packer build --only=hyperv-iso -var-file="$multinodeFile" .\multinode-controller-hyperv.json
+packer build --only=hyperv-iso -var-file="$multinodeFile" .\multinode-infrastructure-hyperv.json
+packer build --only=hyperv-iso -var-file="$multinodeFile" .\multinode-network-hyperv.json
+packer build --only=hyperv-iso -var-file="$multinodeFile" .\multinode-storage-hyperv.json
+
+Import-Packer-VM '.\hyperv\os-deploy\Virtual Machines'
+Import-Packer-VM '.\hyperv\os-compute\Virtual Machines'
+Import-Packer-VM '.\hyperv\os-controller\Virtual Machines'
+Import-Packer-VM '.\hyperv\os-infrastructure\Virtual Machines'
+Import-Packer-VM '.\hyperv\os-network\Virtual Machines'
+Import-Packer-VM '.\hyperv\os-storage\Virtual Machines'
