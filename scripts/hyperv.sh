9d9058c066d7993b7afe59b8ddc8c8e32f8371cd
Geoff Rich <geoff.rich@gmail.com>
2017-08-27 13:11:54 +1000

    Allow checkpointing under hyper v.

--- scripts/hyperv.sh
+++ scripts/hyperv.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -eux
 
+apt-get update
+apt-get install -y --install-recommends linux-virtual-lts-xenial
+apt-get install -y --install-recommends linux-tools-virtual-lts-xenial linux-cloud-tools-virtual-lts-xenial
+
 echo "UseDNS no" >> /etc/ssh/sshd_config
 # Fix for blank screen on hyper-v display
 TARGET_KEY='GRUB_CMDLINE_LINUX_DEFAULT'
